4b06d419a27e19fcc6b0703a387ede4795934146
al_wineland <alwineland@yandex.ru>
2022-06-29 00:31:06 +1100

    progr

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program 
+{
+  static void Main() 
+  {
+    string[] x = {"sdd", "sdsfs", "rty", "sdgsg"};
+    
+    string[] array = Filter3(x);
+    foreach(var i in array)
+    {
+      Console.WriteLine(i);
+    }
+  }
+  static string[] Filter3(string[] array)
+  {
+    int counter = 0;
+    foreach(var i in array)
+    {
+      if(i.Length <= 3) counter++;
+    }
+    string[] result = new string[counter];
+    foreach(var i in array)
+    {
+      if(i.Length <= 3) 
+      {
+        result[counter-1] = i;
+        counter--;
+      }
+    }
+    return result;
+  }
+}
